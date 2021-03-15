9a946f5604750513f57e4cd188704ad890ad50f9
utkukalkanli319 <>
2021-03-15 13:04:41 +0300

    birthdate field değişti, field'ların hepsi dolu olmadığında submit butonu gizlendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -25,13 +26,22 @@ class FormScreen extends StatefulWidget {
 class FormScreenState extends State<FormScreen> {
   String _name;
   String _surname;
-  DateTime _birthDate;
+  DateTime birthDate = DateTime.now();
   String _cityValue;
   String _genderValue;
   String _vaccineTypeValue;
   String _sideEffectValue;
 
+  bool nameFilled = false;
+  bool surnameFilled = false;
+  bool birthDateFilled = false;
+  bool citySelected = false;
+  bool genderSelected = false;
+  bool vaccineTypeSelected = false;
+  bool sideEffectSelected = false;
+
   List<String> sideEffects = [
+    'None',
     'Headache',
     'Fatigue',
     'Fever',
@@ -64,11 +74,16 @@ class FormScreenState extends State<FormScreen> {
         if (value.isEmpty) {
           return 'Name is Required';
         }
-
         return null;
       },
       onSaved: (String value) {
         _name = value;
+      },
+      onChanged: (String value) {
+        setState(() {
+          _name = value;
+          nameFilled = true;
+        });
       },
     );
   }
@@ -81,11 +96,18 @@ class FormScreenState extends State<FormScreen> {
         if (value.isEmpty) {
           return 'Surname is Required';
         }
-
         return null;
       },
       onSaved: (String value) {
         _surname = value;
+      },
+      onChanged: (String value) {
+        setState(
+          () {
+            _surname = value;
+            surnameFilled = true;
+          },
+        );
       },
     );
   }
@@ -99,6 +121,25 @@ class FormScreenState extends State<FormScreen> {
       errorInvalidText: "Entered date is invalid",
       fieldLabelText: "Please enter your birth date",
     );
+  }
+
+  Widget _buildBirthDateField() {
+    return ElevatedButton(
+        onPressed: () => _selectDate(context),
+        child: Text('Select birth date'));
+  }
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: birthDate,
+        firstDate: DateTime(1920, 8),
+        lastDate: DateTime.now());
+    if (picked != null && picked != birthDate)
+      setState(() {
+        birthDate = picked;
+        birthDateFilled = true;
+      });
   }
 
   Widget _buildCity() {
@@ -130,6 +171,7 @@ class FormScreenState extends State<FormScreen> {
         setState(
           () {
             _cityValue = val;
+            citySelected = true;
           },
         );
       },
@@ -165,6 +207,7 @@ class FormScreenState extends State<FormScreen> {
         setState(
           () {
             _genderValue = val;
+            genderSelected = true;
           },
         );
       },
@@ -200,6 +243,7 @@ class FormScreenState extends State<FormScreen> {
         setState(
           () {
             _vaccineTypeValue = val;
+            vaccineTypeSelected = true;
           },
         );
       },
@@ -235,9 +279,43 @@ class FormScreenState extends State<FormScreen> {
         setState(
           () {
             _sideEffectValue = val;
+            sideEffectSelected = true;
           },
         );
       },
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return Visibility(
+      visible: nameFilled &&
+          surnameFilled &&
+          birthDateFilled &&
+          citySelected &&
+          genderSelected &&
+          vaccineTypeSelected &&
+          sideEffectSelected,
+      child: ElevatedButton(
+        child: Text(
+          'Submit',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        onPressed: () {
+          if (!_formKey.currentState.validate()) {
+            return;
+          }
+          _formKey.currentState.save();
+          print(_name);
+          print(_surname);
+          //print(_birthDate.toString());
+          print(birthDate.toString());
+          print(_cityValue);
+          print(_genderValue);
+          print(_vaccineTypeValue);
+          print(_sideEffectValue);
+          //Send to API
+        },
+      ),
     );
   }
 
@@ -267,34 +345,14 @@ class FormScreenState extends State<FormScreen> {
               children: <Widget>[
                 _buildName(),
                 _buildSurname(),
-                _buildBirthDate(),
+                _buildBirthDateField(),
+                //_buildBirthDate(),
                 _buildCity(),
                 _buildGender(),
                 _buildVaccineType(),
                 _buildSideEffect(),
+                _buildSubmitButton(),
                 SizedBox(height: 100),
-                ElevatedButton(
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white70, fontSize: 16),
-                  ),
-                  onPressed: () {
-                    if (!_formKey.currentState.validate()) {
-                      return;
-                    }
-
-                    _formKey.currentState.save();
-
-                    print(_name);
-                    print(_surname);
-                    print(_birthDate);
-                    print(_genderValue);
-                    print(_cityValue);
-                    print(_vaccineTypeValue);
-                    print(_sideEffectValue);
-                    //Send to API
-                  },
-                )
               ],
             ),
           ),
@@ -303,144 +361,3 @@ class FormScreenState extends State<FormScreen> {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Covid19 Vaccine Survey',
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.white70,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hello vaccine people',
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-      ),
-    ));
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
