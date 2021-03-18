3f7643ca1b1ba0eec23803b680bc4b5c0ed612ab
yusufavci99 <yusufavci99@gmail.com>
2021-03-18 16:09:30 +0300

    Updated Android Test

--- AppiumTest/androidTest.py
+++ AppiumTest/androidTest.py
@@ -19,19 +19,29 @@ caps["ensureWebviewsHavePages"] = True
 
 
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+driver.implicitly_wait(20)
+
+def write_text(text):
+    for letter in text:
+        cur_letter_key = ord(letter)
+        driver.press_keycode(cur_letter_key - 68)
 
 wait = WebDriverWait(driver, 20)
-el0 = wait.until(EC.element_to_be_clickable((By.XPATH,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]")))
-el0 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]")
-el0.click()
 
-driver.implicitly_wait(10)
-txtfld = driver.f
+name_field = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]")
+name_field.click()
 
-el3 = wait.until(EC.element((By.XPATH,'//android.widget.Button[@content-desc="Gender"]')))
+sleep(5)
 
-# Boyle yazi yaziyolar normalde ama calismiyor.
-# el0.send_keys("hello")
+# Send Keys Won't Work for some reason.
+# name_field.send_keys('Ahmet')
+write_text('ahmet')
+
+surname_field = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.EditText[2]")
+surname_field.click()
+
+sleep(5)
+write_text("mehmetoglu")
 
 # # Selecting Birth Date
 # select_birth_button = driver.find_element_by_accessibility_id("Select birth date")
@@ -72,6 +82,22 @@ el3.click()
 # Choose Male
 el4 = wait.until(EC.element_to_be_clickable((By.XPATH,'//android.view.View[@content-desc="Male"]')))
 el4.click()
+
+# Click Vaccince Type
+vac_type = driver.find_element_by_accessibility_id("Vaccine Type")
+vac_type.click()
+
+# Choose Type
+astra_zen = driver.find_element_by_accessibility_id("Oxford-Astra-Zeneca")
+astra_zen.click()
+
+# Choose Side Effects
+side_effects = driver.find_element_by_accessibility_id("Side Effects")
+side_effects.click()
+
+# Choose Muscle Pain
+muscle_pain = driver.find_element_by_accessibility_id("Muscle pain")
+muscle_pain.click()
 
 
 sleep(5)
