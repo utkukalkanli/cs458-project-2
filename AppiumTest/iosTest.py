a59120fe0acdf90382a9315d93f5051768347a77
utkukalkanli319 <>
2021-03-19 19:27:13 +0300

    iosTest.py değişiklik

--- AppiumTest/iosTest.py
+++ AppiumTest/iosTest.py
@@ -10,6 +10,13 @@ class LocalIosTest(unittest.TestCase):
     accessKey = "eyJhbGciOiJIUzI1NiJ9.eyJ4cC51IjoxMDk4Mzg3NywieHAucCI6MTA5ODM4NzYsInhwLm0iOjE2MTYwOTk2NDc0MTUsImV4cCI6MTkzMTQ1OTY4MCwiaXNzIjoiY29tLmV4cGVyaXRlc3QifQ.U_cs3cyUv6ESJRAHdYmYzsw5jQOrXsXxEYUseqXD2rc"
     driver = None
 
+    def check_submit_exists(self):
+        self.driver.hide_keyboard()
+        self.driver.implicitly_wait(1)
+        submit_btn = self.driver.find_elements_by_accessibility_id('Submit')
+        self.driver.implicitly_wait(20)
+        return len(submit_btn) == 1
+
     def setUp(self):
         self.dc['reportDirectory'] = self.reportDirectory
         self.dc['reportFormat'] = self.reportFormat
@@ -19,24 +26,77 @@ class LocalIosTest(unittest.TestCase):
         self.dc['bundleId'] = 'com.funksoulbrother.vaccine_survey'
         self.dc['platformName'] = 'ios'
         self.dc['deviceQuery'] = "@os='ios' and @category='PHONE'"
-        self.driver = webdriver.Remote("https://cloud.seetest.io/wd/hub",self.dc)
+        self.driver = webdriver.Remote("https://localhost:4723/wd/hub",self.dc)
 
-    def testQuickStartIosNativeDemo(self):
-        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Username']").send_keys('company')
-        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Password']").send_keys('company')
-        self.driver.find_element_by_xpath("xpath=//*[@text='loginButton']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@text='makePaymentButton']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Phone']").send_keys('1234567')
-        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Name']").send_keys('Jon Snow')
-        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Amount']").send_keys('50')
-        self.driver.find_element_by_xpath("xpath=//*[@text='countryButton']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@text='Switzerland']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@text='sendPaymentButton']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@text='Yes']").click()
-        self.driver.find_element_by_xpath("xpath=//*[@text='logoutButton']").click()
+    def testCase_1(self):
+        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Name']").send_keys('Adnan')
+        self.driver.find_element_by_xpath("xpath=//*[@placeholder='Surname']").send_keys('Ziyagil')
+        self.driver.press_keycode(66)
+
+        # Selecting Birth Date
+        self.driver.find_element_by_accessibility_id("Select birth date").click()
+        self.driver.find_element_by_accessibility_id("Select year").click()
+
+        TouchAction(self.driver)   .long_press(x=707, y=1000)   .move_to(x=707, y=1400)   .release()   .perform()
+        TouchAction(self.driver)   .long_press(x=707, y=1000)   .move_to(x=707, y=1400)   .release()   .perform()
+
+        btn_1990 = self.driver.find_element_by_accessibility_id("1990").click()
+        btn_1990.click()
+        el4 = self.driver.find_element_by_accessibility_id("Next month April 1990")
+        el4.click()
+        el5 = self.driver.find_element_by_accessibility_id("1, Sunday, April 1, 1990")
+        el5.click()
+        el6 = self.driver.find_element_by_accessibility_id("OK")
+        el6.click()
+
+        # Click City
+        city_button = self.driver.find_element_by_accessibility_id("City")
+        city_button.click()
+
+        # Choose Ankara
+        ankara_button = self.driver.find_element_by_accessibility_id("Ankara")
+        ankara_button.click()
+
+        # Click Gender
+        gender_button = self.driver.find_element_by_accessibility_id("Gender")
+        gender_button.click()
+
+        # Choose Male
+        male_button = self.driver.find_element_by_accessibility_id("Male")
+        male_button.click()
+
+        # Click Vaccince Type
+        vac_type = self.driver.find_element_by_accessibility_id("Vaccine Type")
+        vac_type.click()
+
+        # Choose Type
+        astra_zen = self.driver.find_element_by_accessibility_id("Oxford-Astra-Zeneca")
+        astra_zen.click()
+
+        self.assertFalse(self.check_submit_exists())
+
+        # Choose Side Effects
+        side_effects = self.driver.find_element_by_accessibility_id("Side Effects")
+        side_effects.click()
+
+        # Choose Muscle Pain
+        muscle_pain = self.driver.find_element_by_accessibility_id("Muscle pain")
+        muscle_pain.click()
+
+        self.assertTrue(self.check_submit_exists())
+
+
+    def testCase_2(self):
+
+
+    def testCase_3(self):
+
+    def testCase_4(self):
+
+    def testCase_5(self):
 
     def tearDown(self):
-        print ('Report URL: ' + self.driver.capabilities["reportUrl"])
+        print('Report URL: ' + self.driver.capabilities["reportUrl"])
         self.driver.quit()
 
 
